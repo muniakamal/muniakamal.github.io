c4781f369f0936d2b67dd0b85e5b95cc7f371d99
muniakamal <muniakamal7@gmail.com>
2025-01-01 17:04:34 -0500

    added navbar

---
+++ main.js
@@ -0,0 +1,35 @@
+
+/**
+   * 
+   * Initializing function to load global components
+   */
+async function initGlobal() {
+    try {
+        await loadComponent('/navbar.html', 'navbar');
+    } catch (error) {
+        console.error('Error loading components:', error);
+    }
+}
+
+
+/**
+   * 
+   * Function to load component based on path and elementid
+   * @para {string, string} path, id of element
+   */
+async function loadComponent(url, elementId) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const html = await response.text();
+        document.getElementById(elementId).innerHTML = html;
+    } catch (error) {
+        console.error(`Error loading ${url}:`, error);
+    }
+}
+
+
+// Initialize global components
+window.addEventListener("load", initGlobal);
